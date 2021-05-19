24da70f6fdcd0beec9a65d696a9d297840300f1e
TalaDiego <TalaDiego@users.noreply.github.com>
2021-05-19 10:15:14 +0200

    Diego Castaño Freile 1.0

--- src/main/java/controlador/AppNominas.java
+++ src/main/java/controlador/AppNominas.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sistemanominas;
+package controlador;
 
 import controlador.Controlador;
 import modelo.SistemaNominas;
